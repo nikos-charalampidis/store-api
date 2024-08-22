6082471310414a501bff052b78d3a16fca97da5b
bidis <nikcharg4@gmail.com>
2024-08-22 10:43:01 +0300

    test commit

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,3 +1,4 @@
+// error handler
 const errorHandlerMiddleware = async (err, req, res, next) => {
   console.log(err)
   return res.status(500).json({ msg: 'Something went wrong, please try again' })
